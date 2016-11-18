63599d8627b34e654ac5128dfc9f2481413d6530
Nirav Madariya <NiravMadariya@users.noreply.github.com>
2016-11-18 18:57:25 +0530

    Delete jslinkFiles.php

--- jslinkFiles.php
+++
@@ -1,8 +0,0 @@
-	<script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-<!--    <script src="js/Chart.min.js"></script>
-	<script src="js/jquery.prettyPhoto.js"></script>
-	<script src="js/jquery.slicknav.js"></script>-->
-    <script src="js/script.js"></script>
-    <script type="text/javascript">
-	</script>
