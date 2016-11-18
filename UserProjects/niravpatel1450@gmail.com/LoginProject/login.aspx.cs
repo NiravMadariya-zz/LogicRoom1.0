2057b4b3504b9000b7384fc2d50ca855aebf7151
Nirav Madariya <NiravMadariya@users.noreply.github.com>
2016-11-18 18:53:26 +0530

    Delete login.aspx.cs

--- UserProjects/niravpatel1450@gmail.com/LoginProject/login.aspx.cs
+++
@@ -1,8 +0,0 @@
-protected void cmdsubmit_Click(object sender, EventArgs e)
-        {
-            if (txtuname.Text.Equals("BJPAdmin") && txtpass.Text.Equals("Bjp@dmin1"))
-            {
-                Session["uname"] = txtuname.Text;
-                Response.Redirect("index.aspx");
-            }
-        }
