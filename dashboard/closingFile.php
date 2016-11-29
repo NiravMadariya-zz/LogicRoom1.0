5b073085002cff0a1bb46ff27a195575d8a76234
Nirav Madariya <NiravMadariya@users.noreply.github.com>
2016-11-29 23:26:00 +0530

    Update closingFile.php

--- dashboard/closingFile.php
+++ dashboard/closingFile.php
@@ -1,7 +1,7 @@
 <?include("csslinkFiles.php");?>
 <footer class="footer" style="position: absolute; bottom: 0px; width: 100%; padding: 12px 0 12px 0 !important;" >
 	<div class="copyright1" style="float:bottom;margin-bottom:0px;vertical-alignment:center;">
-		Copyright &copy; 2015 LogicRoom.in
+		Copyright &copy; 2015 - 2016 LogicRoom.in
 	</div>
 </footer>
 </div>
